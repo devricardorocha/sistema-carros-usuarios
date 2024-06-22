50cbe4371a4ec11eb69e3919997204985fa9bfc6
ricardo.rocha <ricardo.rocha.3devs@fogas.com.br>
2024-06-22 12:41:03 -0300

    [TASK001] Criar base do projeto

---
+++ src/main/java/com/exemplo/sistemacarrousuario/SistemaDeGestaoDeCarrosEUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.sistemacarrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeCarrosEUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeCarrosEUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exemplo/sistemacarrousuario/SistemaDeGestaoDeCarrosEUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.sistemacarrousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestaoDeCarrosEUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
