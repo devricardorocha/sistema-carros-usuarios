19504822b07dc31d8c117a5c09a17a9b179425b1
ricardo.rocha <ricardo.rocha.3devs@fogas.com.br>
2024-06-26 15:51:03 -0300

    [TASK009] - Disponibilizar Javadoc no Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/sistemacarrousuario-0.0.1-SNAPSHOT.jar
+web: ./mvnw install -Dmaven.javadoc.skip=true && ./mvnw package -DskipTests=true && ./mvnw spring-boot:run
--- src/main/java/com/exemplo/sistemacarrousuario/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/exemplo/sistemacarrousuario/api/security/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.exemplo.sistemacarrousuario.api.security.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,9 +10,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.exemplo.sistemacarrousuario.api.controller.constants.ApiPathConstants;
 import com.exemplo.sistemacarrousuario.api.security.JwtAuthenticationEntryPoint;
@@ -42,10 +47,21 @@ public class WebSecurityConfig {
 	        .addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
 	        .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
 	        .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
+	        .cors(cors -> cors.configurationSource(getCorsConfiguration()))
 	        .authorizeHttpRequests(authorizeHttpRequest());
 	        
         return http.build();
     }
+
+	private CorsConfigurationSource getCorsConfiguration() {
+		return request -> {
+		    CorsConfiguration configuration = new CorsConfiguration();
+		    configuration.setAllowedOrigins(Arrays.asList("*"));
+		    configuration.setAllowedMethods(Arrays.asList("*"));
+		    configuration.setAllowedHeaders(Arrays.asList("*"));
+		    return configuration;
+		};
+	}
 
 	private Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizeHttpRequest() {
 		return authorizationManagerRequestMatcherRegistry ->
